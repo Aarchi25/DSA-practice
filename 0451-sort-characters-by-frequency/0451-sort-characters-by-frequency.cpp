7b6d6515a18d1e9924c2670996681e1e1e5100e9
Aarchi25 <aarchiisrani2504@gmail.com>
2026-09-09 19:36:02 +0530

    Time: 5 ms (28.96%), Space: 17.1 MB (5.84%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -3,15 +3,15 @@ public:
     string frequencySort(string s) {
         unordered_map<char,int>freq;
         for(auto  ch:s) freq[ch]++;
-        vector<pair<char,int>>freqArr;
-        for(auto[ch,fq]:freq) freqArr.push_back({ch,fq});
-        auto cmp=[&](pair<char,int>&a,pair<char,int>&b){
-            return a.second>b.second;
-        };
-        sort(freqArr.begin(),freqArr.end(),cmp);
-        string ans="";
-        for(auto[ch,fq]:freqArr) ans.append(fq,ch);
-        return ans;
 
+        vector<vector<char>>bucketData(s.length()+1);
+        for(auto [ch,fq]:freq) bucketData[fq].push_back(ch);
+
+        string ans="";
+        for(int fq=s.length();fq>=1;fq--){
+            for(auto ch:bucketData[fq])
+              ans.append(fq,ch);
+        }
+        return ans;
     }
 };
